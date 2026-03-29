13048783641856c807ddcd69fe43a68a921c8a84
YLSWKSY <pratinumhawksoul217@gmail.com>
2026-03-29 23:04:43 +0900

    Add files via upload

---
+++ javascript編/javascript編/src/js/main.js
@@ -0,0 +1,81 @@
+// console.log("ZeroPlus Gate");
+
+// console.log(1);
+// console.log("1");
+
+// console.log(1 + 4);
+// console.log(3 - 2);
+// console.log(2 * 2);
+// console.log(6 / 2);
+
+// console.log("ZeroPlus" + "Gate");
+
+// 変数
+// var zp = "ZeroPlus";
+// console.log(zp);
+
+// var 変数名 = 値;
+
+// let zpg = "ZeroPlus Gate"
+// console.log(zpg);
+
+// var 再代入、再定義
+// let 再代入のみ
+// const どっちもできない
+
+//再代入
+// var fruit = "りんご";
+// fruit = "バナナ";
+
+// console.log(fruit);
+
+//再定義
+// var fruit = "パイナップル";
+// console.log(fruit);
+
+// let fruit = "リンゴ";
+// fruit = "バナナ";
+
+// let fruit = "パイナップル";
+// console.log(fruit);
+
+// letが一般的
+
+// 変数の使い道
+// 例-計算時の一時記憶
+
+// let total = 0;
+
+// total = 1200;
+// total = total + 2200;
+// total = total + 800;
+// total = total + 4100;
+
+// console.log(total);
+
+// 定数は後から再代入できない
+
+// const zp = "ZeroPLus";
+// console.log(zp);
+
+// zp = "ZeroPlus Gate";
+// console.log(zp);
+
+// 変化が想定されないかつ何度も使いまわされる値は定数
+
+// console.log(zp + "Gate");
+// console.log(zp + "Career");
+// console.log(zp + "Club");
+
+// 比較演算子 true または　false の真偽値を返す
+// a < b
+// a <= b
+// a > b
+// a >= b
+// a == b
+// a === b 厳密に
+// a != b
+// a !== b 厳密に
+
+console.log(10 < 5);
+console.log(10 <= 5);
